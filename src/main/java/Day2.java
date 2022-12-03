d8a76e12353e6477c776139a4126181c80d825ce
nikita.shvinagir <nikita.shvinagir@gmail.com>
2022-12-03 20:39:52 +0100

    Implement Day2 class returning points against Elves in rock-paper-scissors

---
+++ src/main/java/Day2.java
@@ -0,0 +1,126 @@
+import java.text.MessageFormat;
+import java.util.List;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        // Day2 part 1
+//        FileReader<Round> fileReader = new FileReader<>("day2", Round::decode);
+        // Day2 part 2
+        FileReader<Round> fileReader = new FileReader<>("day2", Round::elfDecode);
+        System.out.println("Points = " + getWinningPoints(fileReader.readFile()));
+    }
+
+    private static long getWinningPoints(List<Round> rounds) {
+        return rounds.stream().map(round -> {
+            Result result = round.me.compare(round.opponent);
+            return result.points + round.me.getPoints();
+        }).reduce(Integer::sum).orElse(0);
+    }
+
+    /**
+     * Returns a Hand object that gives the given {@code result} against the given {@code opponent}
+     */
+    static Hand getHandFor(Result result, Hand opponent) {
+        return switch (result) {
+            case LOSS -> opponent.winsTo();
+            case DRAW -> opponent;
+            case WIN -> opponent.loosesTo();
+        };
+    }
+
+    enum Hand {
+        ROCK, PAPER, SCISSORS;
+
+        static Hand from(Character character) {
+            return switch (character) {
+                case 'A', 'X' -> ROCK;
+                case 'B', 'Y' -> PAPER;
+                case 'C', 'Z' -> SCISSORS;
+                default -> throw new IllegalArgumentException(
+                        MessageFormat.format("Cannot convert {0} to a Hand", character));
+            };
+
+        }
+
+        int getPoints() {
+            return this.ordinal() + 1;
+        }
+
+        Hand loosesTo() {
+            return switch (this) {
+                case ROCK -> PAPER;
+                case PAPER -> SCISSORS;
+                case SCISSORS -> ROCK;
+            };
+        }
+
+        Hand winsTo() {
+            return switch (this) {
+                case ROCK -> SCISSORS;
+                case PAPER -> ROCK;
+                case SCISSORS -> PAPER;
+            };
+        }
+
+        Result compare(Hand opponent) {
+            if (this.equals(opponent)) {
+                return Result.DRAW;
+            }
+            if (this.equals(ROCK)) {
+                if (opponent.equals(SCISSORS)) {
+                    return Result.WIN;
+                }
+            } else if (this.equals(PAPER)) {
+                if (opponent.equals(ROCK)) {
+                    return Result.WIN;
+                }
+
+            } else if (this.equals(SCISSORS)) {
+                if (opponent.equals(PAPER)) {
+                    return Result.WIN;
+                }
+            }
+            return Result.LOSS;
+        }
+    }
+
+    enum Result {
+        WIN(6), DRAW(3), LOSS(0);
+        private final int points;
+
+        Result(int points) {
+            this.points = points;
+        }
+
+        static Result decode(Character character) {
+            return switch (character) {
+                case 'X' -> LOSS;
+                case 'Y' -> DRAW;
+                case 'Z' -> WIN;
+                default -> throw new IllegalArgumentException(
+                        MessageFormat.format("Cannot convert {0} to a Result", character));
+            };
+        }
+    }
+
+    record Round(Hand opponent, Hand me) {
+        static Round decode(String s) {
+            String value = s.strip();
+            if (value.length() != 3 || value.charAt(1) != ' ') {
+                throw new IllegalArgumentException(MessageFormat.format("Wrong file format: ", value));
+            }
+            return new Round(Hand.from(value.charAt(0)), Hand.from(value.charAt(2)));
+        }
+
+        static Round elfDecode(String s) {
+            String value = s.strip();
+            if (value.length() != 3 || value.charAt(1) != ' ') {
+                throw new IllegalArgumentException(MessageFormat.format("Wrong file format: ", value));
+            }
+            Hand opponent = Hand.from(value.charAt(0));
+            Hand me = getHandFor(Result.decode(value.charAt(2)), opponent);
+            return new Round(opponent, me);
+        }
+    }
+}
