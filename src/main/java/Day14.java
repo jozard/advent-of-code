495870907dad89f4ae8f2786dc417aa718630fd9
nikita.shvinagir <nikita.shvinagir@gmail.com>
2023-01-06 23:00:21 +0100

    Implement Day 14 part pouring sand in a cave

---
+++ src/main/java/Day14.java
@@ -0,0 +1,187 @@
+import java.awt.*;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+
+        FileReader<Wall> fileReader = new FileReader<>("day14",
+                (index, line) -> lineToWall(line));
+        List<Wall> walls = fileReader.readFile();
+        Set<Point> wallPoints = new HashSet<>();
+        walls.forEach(wall -> {
+            Iterator<Point> pointIterator = wall.pinPoints.iterator();
+            Point current = pointIterator.next();
+            while (pointIterator.hasNext()) {
+                Point next = pointIterator.next();
+                Set<Point> result = getWallPoints(current, next);
+                wallPoints.addAll(result);
+                current = next;
+            }
+        });
+        System.out.println("wallPoints = " + wallPoints);
+        int minX = wallPoints.stream().map(point -> point.x).min(Integer::compare).orElseThrow();
+        int maxX = wallPoints.stream().map(point -> point.x).max(Integer::compare).orElseThrow();
+        int minY = wallPoints.stream().map(point -> point.y).min(Integer::compare).orElseThrow();
+        int maxY = wallPoints.stream().map(point -> point.y).max(Integer::compare).orElseThrow();
+
+        int xShift = 1 - minX; // shift x coordinates towards 0; add extra x position to the left
+        wallPoints.forEach(point -> point.setLocation(point.x + xShift, point.y));
+
+        int caveWidth = maxX - minX + 3;
+        int caveHeight = maxY + 2;
+        int pouringX = 500 + xShift;
+        Cave cave = new Cave(caveWidth, caveHeight, wallPoints);
+
+        cave.print();
+        System.out.println();
+
+        int unitCount = 0;
+        int x = pouringX;
+        int y = 0;
+        while (y < maxY) {
+
+            if (cave.getTile(x, y + 1).isFree()) {
+                y++;
+            } else if (cave.getTile(x - 1, y + 1).isFree()) {
+                x--;
+                y++;
+            } else if (cave.getTile(x + 1, y + 1).isFree()) {
+                x++;
+                y++;
+            } else {
+                cave.setTile(new Tile(Tile.Sprite.SAND), x, y);
+
+                cave.print();
+                System.out.println();
+
+                unitCount++;
+                x = pouringX;
+                y = 0;
+            }
+        }
+        System.out.println("unitCount = " + unitCount);
+
+    }
+
+    private static Wall lineToWall(String value) {
+        LinkedList<Point> pinPoints = new LinkedList<>(Arrays.stream(value.split(" -> ")).map(pinPoint -> {
+            List<Integer> numbers = Arrays.stream(pinPoint.split(",")).map(Integer::parseInt).toList();
+            return new Point(numbers.get(0), numbers.get(1));
+        }).toList());
+        return new Wall(pinPoints);
+    }
+
+    static Set<Point> getWallPoints(Point from, Point to) {
+        int start;
+        int end;
+        Function<Integer, Point> toPoint;
+        if (from.x < to.x) {
+            start = from.x;
+            end = to.x + 1;
+            toPoint = item -> new Point(item, from.y);
+        } else if (from.x > to.x) {
+            start = to.x;
+            end = from.x + 1;
+            toPoint = item -> new Point(item, from.y);
+        } else if (from.y < to.y) {
+            start = from.y;
+            end = to.y + 1;
+            toPoint = item -> new Point(from.x, item);
+        } else {
+            start = to.y;
+            end = from.y + 1;
+            toPoint = item -> new Point(from.x, item);
+        }
+        return IntStream.range(start, end).boxed().map(toPoint).collect(
+                Collectors.toSet());
+    }
+
+    private static void assertHorizontalWall(Point start, Point end) {
+        if (start.y != end.y) {
+            throw new IllegalArgumentException(
+                    MessageFormat.format("Illegal wall start and end points: {}, {}", start, end));
+        }
+    }
+
+
+    record Wall(LinkedList<Point> pinPoints) {
+    }
+
+    static class Cave {
+
+        private final Tile[][] tiles; // [y][x]
+
+        public Cave(int width, int height, Set<Point> wallPoints) {
+            tiles = new Tile[height][width];
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+                    int finalI = i;
+                    int finalJ = j;
+                    if (wallPoints.stream().anyMatch(item -> item.x == finalI && item.y == finalJ)) {
+                        this.setTile(new Tile(Tile.Sprite.STONE), i, j);
+                    } else {
+                        this.setTile(new Tile(Tile.Sprite.AIR), i, j);
+                    }
+                }
+            }
+        }
+
+        public void setTile(Tile tile, int x, int y) {
+            tiles[y][x] = tile;
+        }
+
+        public Tile getTile(int x, int y) {
+            return tiles[y][x];
+        }
+
+        public void print() {
+            for (Tile[] tile : tiles) {
+                for (Tile value : tile) {
+                    System.out.print(value.toString());
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
+
+class Tile {
+
+    char sprite = '.';
+
+    public Tile(Sprite sprite) {
+        this.sprite = spriteToChar(sprite);
+    }
+
+    private static char spriteToChar(Sprite sprite) {
+        return switch (sprite) {
+            case AIR -> '.';
+            case SAND -> '0';
+            case STONE -> '#';
+        };
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = spriteToChar(sprite);
+    }
+
+    public boolean isFree() {
+        return sprite == '.';
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(sprite);
+    }
+
+    enum Sprite {
+        AIR, STONE, SAND
+    }
+}
