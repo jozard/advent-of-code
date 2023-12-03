060c458a46518206989c28257988a6f57523f4fb
jozard <nikita.shvinagir@gmail.com>
2023-12-03 15:39:41 +0100

    Add part2 day 2 2023

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -1,7 +1,9 @@
+import math
 lines = open("data/day_2.data", "r").readlines()
 
 id_sum = 0
 
+# part one cube count limits
 bag_items = {'red': 12, 'green': 13, 'blue': 14}
 
 def verify_cube_set(cube_set):
@@ -10,10 +12,20 @@ def verify_cube_set(cube_set):
     colour = None
     (count,s,colour) = cube_set.strip().partition(' ')
     return bag_items.get(colour) >= int(count)
+    
+def get_cube_set_colour_count(cube_set):
+    count = None
+    s = None
+    colour = None
+    (count,s,colour) = cube_set.strip().partition(' ')
+    return (colour,count)    
 
 def verify_handful(handful):
     if all(verify_cube_set(cube_set) for cube_set in handful.split(', ')):
         return True
+
+def get_handful_dict(handful):
+    return {item[0]: int(item[1]) for item in list(map(get_cube_set_colour_count, handful.split(', ')))}
 
 for line in lines:
     prefix = None
@@ -22,8 +34,18 @@ for line in lines:
     (prefix,separator,game_content) = line.strip().partition(': ')
    
     handfuls = game_content.split('; ')
-    if all(verify_handful(handful) for handful in handfuls): 
-        id_sum += lines.index(line)+1
+    
+    #part one
+    # if all(verify_handful(handful) for handful in handfuls):
+        # id_sum += lines.index(line)+1
+        
+    game_dict = {'red':1,'blue':1,'green':1}    
+    for handful in handfuls:
+        handful_dict = get_handful_dict(handful)
+        for handful_item in handful_dict.items():
+            if handful_item[1] > game_dict[handful_item[0]]:
+                game_dict[handful_item[0]]=handful_item[1]
+    id_sum+= math.prod(game_dict.values())        
     
 
 print(id_sum)
