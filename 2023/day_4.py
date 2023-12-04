280ad6510ca01fb7f426d6ee4e86b909428233b4
jozard <nikita.shvinagir@gmail.com>
2023-12-04 23:49:00 +0100

    Add part#3 to day 4 2023

--- 2023/day_4.py
+++ 2023/day_4.py
@@ -2,6 +2,8 @@ import math
 import re
 lines = open("data/day_4.data", "r").readlines()
 points = 0
+scratch_cards = [1] * len(lines)
+line_index = 1
 for line in lines:
     print(line)
     item = line.partition(':')[2].strip().partition(' | ')
@@ -11,12 +13,20 @@ for line in lines:
     card['numbers'] = [int(num) for num in item[2].strip().split(' ') if len(num)>0]
     print(card)
     acc = 0
+    matches = 0
     for win in card['wins']:
        if (win in card['numbers']):
            acc = 1 if acc == 0 else acc*2
+           # part 2 only counts matches
+           matches+=1
+           
     points += acc
+    if matches>0:
+        for x in range(1, matches+1):
+            scratch_cards[line_index-1+x] = scratch_cards[line_index-1+x] + scratch_cards[line_index-1]
     
-print(points)        
+    line_index+=1
     
     
-    
+print(points)     
+print(sum(scratch_cards))   
