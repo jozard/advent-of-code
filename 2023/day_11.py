37fe58bf8330e1dc00f8ed212ba85444dc6b1e55
jozard <nikita.shvinagir@gmail.com>
2023-12-12 23:50:41 +0100

    Add part 1 day 11 2023

---
+++ 2023/day_11.py
@@ -0,0 +1,37 @@
+import re
+import time
+
+with open('data/day_11.data') as f:
+    start_time = time.time()
+    space = []
+    for line in f:
+        space.append(line.strip())
+        
+    empty_rows = []
+    for i in range(len(space)):
+        if not '#' in space[i]:
+            empty_rows.append(i)
+            
+    empty_cols = []        
+    for i in range(len(space[0])): # num of columns
+        col = [row[i] for row in space]
+        if not '#' in col:
+            empty_cols.append(i)      
+            
+    galaxies = []
+    for i in range(len(space)):
+        for j in range(len(space[i])):
+            if space[i][j]=='#':
+                galaxies.append((i+sum(item<i for item in empty_rows),j+sum(item<j for item in empty_cols)))                            
+                            
+    print(galaxies)
+    s = 0
+    for a in range(len(galaxies)):
+        for b in range(a+1, len(galaxies)):
+            m = galaxies[a]
+            n = galaxies[b]
+            
+            s += abs(m[1]-n[1]) + abs(m[0]-n[0])                          
+
+    print(f'{time.time() - start_time}s: {s}')
+    f.close()
