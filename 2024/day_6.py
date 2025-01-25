cfeaeefb5689a47bb196a069744716ab43476094
jozard <nikita.shvinagir@gmail.com>
2025-01-25 13:09:53 +0100

    Implement Day 6 2024 part 2

--- 2024/day_6.py
+++ 2024/day_6.py
@@ -9,13 +9,13 @@ with open("data/day_6.data", "r") as f:
         l = line.strip()
         lab_map.append(l)
 
-start = (0, 0)
+arrow = (0, 0)
 for j, line in enumerate(lab_map):
     i = line.find('^')
     if i >= 0:
-        start = (j, i)
+        arrow = (j, i)
         break
-print(start)
+print(arrow)
 
 
 def move(s, d):
@@ -31,33 +31,64 @@ def change_direction(d):
     return 'e' if d == 'n' else ('w' if d == 's' else ('s' if d == 'e' else 'n'))
 
 
-# part 1
-result = 0
-# print(lab_map)
-direction = 'n'  # e,s,w
-visited_counted[start] = 1
-
-
-def find_next(s, d):
+def find_next(s, d, m):
     n = move(s, d)
-    while lab_map[n[0]][n[1]] == '#':
+    while m[n[0]][n[1]] == '#' or m[n[0]][n[1]] == '0':
         d = change_direction(d)
         n = move(s, d)
     return n, d
 
 
 def is_last(current, d) -> bool:
-    nxt = move(start, direction)
+    nxt = move(current, d)
     return 0 > nxt[0] or nxt[0] >= len(lab_map) or 0 > nxt[1] or nxt[1] >= len(lab_map[0])
 
 
+# part 1
+result = 0
+start = tuple(arrow)
+
+direction = 'n'  # e,s,w
+visited_counted[start] = 1
 while True:
     if is_last(start, direction):
         break
-    start, direction = find_next(start, direction)
+    start, direction = find_next(start, direction, lab_map)
     visited_counted[start] = 1 if not start in visited_counted else visited_counted[start] + 1
 
 result = len(visited_counted.values())
 print(result)
 
 # part 2
+test_positions = list(visited_counted.keys())
+test_positions.remove(arrow)
+
+result = 0
+visited_directions: dict[tuple, set[str]]
+for pos in test_positions:
+    visited_directions = dict()  # this dictionary will contain set of directions
+    m = list(lab_map)  # copy the original map to m
+    s = list(m[pos[0]])  # get a row in m matching pos row
+    s[pos[1]] = '0'  # put an obstacle to the pos position in the m
+    m[pos[0]] = "".join(s)  # replace row in m with a row with obstacle
+    start = tuple(arrow)
+
+    direction = 'n'  # e,s,w
+    start_set = set(direction)
+    visited_directions[start] = start_set
+
+    while True:
+        if is_last(start, direction):  # if the next move is out of map, current obstacle will not result into a loop
+            break
+        start, direction = find_next(start, direction, m)
+        if not start in visited_directions:
+            visited_directions[start] = set(direction)
+        else:
+            vd = visited_directions[start]  # get visited directions at start position
+            if direction in vd:  # this position has already been visited from the same direction -> looped
+                result = result + 1
+                break
+            else:
+                visited_directions[start].add(direction)
+
+print(result)
